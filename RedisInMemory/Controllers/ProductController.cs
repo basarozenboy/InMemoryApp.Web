c2b29c1979fa517536a8a128db4a402eec1030be
Basar Ozenboy <pashaoo@hotmail.com>
2024-08-04 18:11:57 +0300

    güncelleme

---
+++ RedisInMemory/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace RedisInMemory.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache memoryCache;
+
+        public ProductController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
